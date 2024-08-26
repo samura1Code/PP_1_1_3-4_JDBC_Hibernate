3c16a1557909c9ab8abbd6396abaa61d08b17db3
Valeriy Makurin <mr.makurin@icloud.com>
2024-08-26 23:36:11 +0300

    Set up database connection and schema
    - Cloned repository
    - Created database connection and schema using Workbench
    - Tested database connectivity in IntelliJ using built-in tools

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,9 @@
 package jm.task.core.jdbc;
+import java.sql.Connection;
+import java.sql.Driver;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
     }
 }
