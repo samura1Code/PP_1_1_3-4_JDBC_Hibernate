987a8036c02f7bf1578b8e9c63fd37e89a554cda
Valeriy Makurin <mr.makurin@icloud.com>
2024-08-27 00:34:14 +0300

    Created Util class for JDBC connection: - Added getConnection() method for establishing a database connection. - Used standard MySQL driver com.mysql.cj.jdbc.Driver. - Added constants URL, USER, and PASSWORD for connection configuration. - Handled SQLException and ClassNotFoundException.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,11 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.util.Util;
+
 import java.sql.Connection;
 import java.sql.Driver;
 
 public class Main {
     public static void main(String[] args) {
-
+        Connection connection = Util.getConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
+import java.sql.Driver;
 
 public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/kata_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "12TSsKt@";
 
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Create the connection
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connection successful!");
+            }
+            connection.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Connection failed!");
+        }
+
+        return connection;
+    }
 }
