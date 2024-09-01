c92e9532074b2375203268dd46b16f2fa9945a16
Valeriy Makurin <mr.makurin@icloud.com>
2024-09-01 15:02:57 +0300

    Reverted final solution for task 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,36 +1,35 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+//import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-//         UserServiceImpl userService = new UserServiceImpl();
-//         userService.createUsersTable();
-//        userService.saveUser("Dmitriy", "Petrov", (byte) 40);
-//        userService.saveUser("Vitaliy", "Vitaliev", (byte) 25);
-//        userService.saveUser("Petr", "Perov", (byte) 80);
-//        userService.saveUser("Valeriy", "Makurin", (byte) 29);
-//        for (int i = 0; i < userService.getAllUsers().size(); i++) {
-//           System.out.println(userService.getAllUsers().get(i));
-//
-//        }
-//
+         UserServiceImpl userService = new UserServiceImpl();
+         userService.createUsersTable();
+         userService.saveUser("Dmitriy", "Petrov", (byte) 40);
+         userService.saveUser("Vitaliy", "Vitaliev", (byte) 25);
+         userService.saveUser("Petr", "Perov", (byte) 80);
+         userService.saveUser("Valeriy", "Makurin", (byte) 29);
+         for (int i = 0; i < userService.getAllUsers().size(); i++) {
+           System.out.println(userService.getAllUsers().get(i));
+         }
+
 //       userService.cleanUsersTable();
 //       userService.dropUsersTable();
 
-        UserService userService = new UserServiceImpl();
-            userService.createUsersTable();
-          userService.saveUser("Valeriy", "Makurin", (byte) 29);
-          userService.saveUser("Vasiliy", "Podkolzin", (byte) 22);
-          userService.saveUser("Vasiliy", "Mikhail", (byte) 21);
-          userService.saveUser("Andrey", "Petrov", (byte) 21);
-        for(int i = 0; i < userService.getAllUsers().size(); i++) {
-            System.out.println(userService.getAllUsers().get(i));
-        }
-        userService.removeUserById(1);
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        UserService userService = new UserServiceImpl();
+//            userService.createUsersTable();
+//          userService.saveUser("Valeriy", "Makurin", (byte) 29);
+//          userService.saveUser("Vasiliy", "Podkolzin", (byte) 22);
+//          userService.saveUser("Vasiliy", "Mikhail", (byte) 21);
+//          userService.saveUser("Andrey", "Petrov", (byte) 21);
+//        for(int i = 0; i < userService.getAllUsers().size(); i++) {
+//            System.out.println(userService.getAllUsers().get(i));
+//        }
+//        userService.removeUserById(1);
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+//import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -9,37 +9,37 @@ import java.util.List;
 
 public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
 
- private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
+ //private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        userDaoHibernate.createUsersTable();
-//        super.createUsersTable();
+//        userDaoHibernate.createUsersTable();
+        super.createUsersTable();
     }
 
 
     public void dropUsersTable() {
-        userDaoHibernate.dropUsersTable();
-//        super.dropUsersTable();
+//        userDaoHibernate.dropUsersTable();
+        super.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoHibernate.saveUser(name, lastName, age);
-//        super.saveUser(name, lastName, age);
+//        userDaoHibernate.saveUser(name, lastName, age);
+        super.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDaoHibernate.removeUserById(id);
-//      super.removeUserById(id);
+//        userDaoHibernate.removeUserById(id);
+      super.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() {
-//        return super.getAllUsers();
-          return userDaoHibernate.getAllUsers();
+        return super.getAllUsers();
+//          return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-//        super.cleanUsersTable();
-          userDaoHibernate.cleanUsersTable();
+        super.cleanUsersTable();
+//          userDaoHibernate.cleanUsersTable();
     }
 }
